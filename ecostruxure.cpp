f345305b2634a3f5977777deda21b7666c5964aa
Frank Schumann <frank.schumann.76@gmail.com>
2022-12-27 23:14:33 +0100

    remove file ecostruxure.cpp was created with ecplise to create simple CMake project

--- ecostruxure.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "config.h"
-
-int main(int argc, char **argv) {
-	std::cout << "Hello World" << std::endl;
-	std::cout << "Version " << ecostruxure_VERSION_MAJOR << "." << ecostruxure_VERSION_MINOR << std::endl;
-	return 0;
-}
