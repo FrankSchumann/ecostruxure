9816fd1039f754a6891fe5d4454d3ea274f9837f
Frank Schumann <frank.schumann.76@gmail.com>
2023-01-08 22:13:32 +0100

    call executeable ecostruxureTest

--- test/src/factory/EcoStruxureFactoryTest.cpp
+++ test/src/factory/EcoStruxureFactoryTest.cpp
@@ -1,14 +1,14 @@
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
-TEST(EcoStruxureTest, BasicAssertions) {
+TEST(EcoStruxureFactoryTest, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
 
-TEST(EcoStruxureTest, Frank1)
+TEST(EcoStruxureFactoryTest, Frank1)
 {
 	GTEST_SUCCEED();
 }
